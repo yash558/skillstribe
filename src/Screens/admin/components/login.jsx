f6670e1a32b24a3fba84527a3137ef652ac8ad09
yash sharma <76558099+yash558@users.noreply.github.com>
2023-03-09 17:55:12 +0530

    third commit

--- src/Screens/admin/components/login.jsx
+++ src/Screens/admin/components/login.jsx
@@ -4,7 +4,7 @@ import { useAlert } from 'react-alert';
 import { Button } from 'semantic-ui-react';
 import axios from 'axios';
 import './style.scss';
-import * as Alaio from '../../../components/service/alaio';
+import * as Alaio from '../../../service/alaio';
 import * as aladinjs from 'aladinjs';
 
 function LoginPage() {
@@ -15,55 +15,55 @@ function LoginPage() {
 	let history = useHistory();
 	const alert = useAlert();
 
-	const onSubmit = async() => {
+	const onSubmit = async () => {
 		//If user uses a mnemonic phrase
-		if(code.includes(" ")) {
+		if (code.includes(" ")) {
 			code = await encryptMnemonic(code, pass).catch(err => {
 				console.log(err)
 			});
 		}
 
-		await axios.post('https://euapi.alacritys.net/users/login', {password: pass, mnemonic_code: code})	//mainnet
-		// await axios.post('https://euapitest.alacritys.net/users/login', {email, password: pass, mnemonic_code: code})	//testnet
-		.then(async(response) => {
-			console.log(response.data.data);
-			let username = '';
-			if(response.status === 200){
-				let activeKey = await Alaio.getUserKeys(code, pass).then(res => res.data.data.activeKey);
-				localStorage.setItem('keys', JSON.stringify(activeKey))
-				let rpc = Alaio.makeRPC();
-				username = await rpc.history_get_key_accounts(activeKey.pub_key).then( async(response) => {
-					return response.account_names[0];
-				})
-			}
-
-			if(username === "skillsadmin"){
-				let data = {
-					accPrivateKey: response.data.data.accPrivateKey,
-					account: response.data.data.account,
-					mnemonicCode: response.data.data.mnemonicCode,
-					profileInfo: response.data.data.profileInfo,
-					publicKeychain: response.data.data.publicKeychain,
-					username: username
+		await axios.post('https://euapi.alacritys.net/users/login', { password: pass, mnemonic_code: code })	//mainnet
+			// await axios.post('https://euapitest.alacritys.net/users/login', {email, password: pass, mnemonic_code: code})	//testnet
+			.then(async (response) => {
+				console.log(response.data.data);
+				let username = '';
+				if (response.status === 200) {
+					let activeKey = await Alaio.getUserKeys(code, pass).then(res => res.data.data.activeKey);
+					localStorage.setItem('keys', JSON.stringify(activeKey))
+					let rpc = Alaio.makeRPC();
+					username = await rpc.history_get_key_accounts(activeKey.pub_key).then(async (response) => {
+						return response.account_names[0];
+					})
 				}
-				localStorage.setItem('admin', JSON.stringify(data))
-				history.push('/admin/dashboard');
-			}
-			else{
-				alert.error("Looks like you aren't an admin!")
-			}
-		})
-		.catch(error => {
-			alert.error(`An error occured: ${error}`);
-			return;
-		})
+
+				if (username === "skillsadmin") {
+					let data = {
+						accPrivateKey: response.data.data.accPrivateKey,
+						account: response.data.data.account,
+						mnemonicCode: response.data.data.mnemonicCode,
+						profileInfo: response.data.data.profileInfo,
+						publicKeychain: response.data.data.publicKeychain,
+						username: username
+					}
+					localStorage.setItem('admin', JSON.stringify(data))
+					history.push('/admin/dashboard');
+				}
+				else {
+					alert.error("Looks like you aren't an admin!")
+				}
+			})
+			.catch(error => {
+				alert.error(`An error occured: ${error}`);
+				return;
+			})
 	}
 
-	const encryptMnemonic = async(mnemonic, password) => {
+	const encryptMnemonic = async (mnemonic, password) => {
 		return aladinjs.encryptMnemonic(mnemonic, password);
 	}
 
-	return(
+	return (
 		<div className="my-container">
 			<div>
 				<form>
@@ -76,25 +76,25 @@ function LoginPage() {
 					</label> */}
 					<label htmlFor="">
 						<h4>Recovery Code</h4>
-						<textarea 
-						value={code}
-						onChange={(e) => setCode(e.target.value)}
-						id="code-area" 
-						type="text"/>
+						<textarea
+							value={code}
+							onChange={(e) => setCode(e.target.value)}
+							id="code-area"
+							type="text" />
 					</label>
 					<label htmlFor="">
 						<h4>Password</h4>
-						<input 
-						value={pass}
-						onChange={(e) => setPass(e.target.value)}
-						type="password"/>
+						<input
+							value={pass}
+							onChange={(e) => setPass(e.target.value)}
+							type="password" />
 					</label>
 					<div className="btn-container">
 						<Button
-						type="button"
-						disabled={code === '' || pass === ''}
-						className="btn" 
-						onClick={onSubmit}>Sign In</Button>
+							type="button"
+							disabled={code === '' || pass === ''}
+							className="btn"
+							onClick={onSubmit}>Sign In</Button>
 					</div>
 				</form>
 			</div>
--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,118 +1,118 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import yoga from "../../assets/Yoga.png";
 import "./create.css";
 import axios from 'axios'
 import { Link } from "react-router-dom";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useAlert } from 'react-alert'
 import * as aladinjs from 'aladinjs'
-import * as Alaio from '../../components/service/alaio'
+import * as Alaio from '../../service/alaio'
 
 const Create = () => {
   let signUpLink = 'https://demo.alacritys.net/';
   const [username, setUsername] = useState('')
-	const [email, setEmail] = useState('')
-	const [pass, setPass] = useState('')
-	let   [mnemonic_code, set_mnemonic_code] = useState('')
-	const [error, setError] = useState('')
-	const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+  let [mnemonic_code, set_mnemonic_code] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
-	let history = useNavigate()
-	const alert = useAlert()
+  let history = useNavigate()
+  const alert = useAlert()
 
-	const getUser = async (username) => {
-		setLoading(true)
-		if (username !== '' || username !== null) {
-			let user = await Alaio.loadUser(username)
-			if (user) {
-				let storage = JSON.parse(localStorage.getItem('User'))
-				if (user.person === storage.username) {
-					alert.success('Successfully Logged In!')
-					await localStorage.setItem('role', user.role)
-					await localStorage.setItem('image', user.imageURL)
-					history.push('/account')
-				} else setError('An Error Occured, please try again!')
-			} else {
-				history.push('account-info-setup')
-			}
-		} else setError('No username set')
-	}
+  const getUser = async (username) => {
+    setLoading(true)
+    if (username !== '' || username !== null) {
+      let user = await Alaio.loadUser(username)
+      if (user) {
+        let storage = JSON.parse(localStorage.getItem('User'))
+        if (user.person === storage.username) {
+          alert.success('Successfully Logged In!')
+          await localStorage.setItem('role', user.role)
+          await localStorage.setItem('image', user.imageURL)
+          history.push('/account')
+        } else setError('An Error Occured, please try again!')
+      } else {
+        history.push('account-info-setup')
+      }
+    } else setError('No username set')
+  }
 
-	const onSubmit = async () => {
-		//If user uses a mnemonic phrase
-		if (mnemonic_code.includes(' ')) {
-			mnemonic_code = await encryptMnemonic(mnemonic_code, pass).catch(
-				(err) => {
-					console.log(err)
-				},
-			)
-		}
+  const onSubmit = async () => {
+    //If user uses a mnemonic phrase
+    if (mnemonic_code.includes(' ')) {
+      mnemonic_code = await encryptMnemonic(mnemonic_code, pass).catch(
+        (err) => {
+          console.log(err)
+        },
+      )
+    }
 
-		await axios
-			.post('https://euapi.alacritys.net/users/login', {
-				email,
-				password: pass,
-				mnemonic_code,
-			})
-			.then(async (response) => {
-				if (response.status === 200) {
-					let activeKey = await Alaio.getUserKeys(
-						mnemonic_code,
-						pass,
-					).then((res) => res.data.data.activeKey)
-					localStorage.setItem('keys', JSON.stringify(activeKey))
-					let rpc = Alaio.makeRPC()
-					let username = await rpc
-						.history_get_key_accounts(activeKey.pub_key)
-						.then(async (response) => {
-							return response.account_names[0]
-						})
-					if (username !== '') {
-						console.log('res', response)
-						await localStorage.removeItem('User')
-						await setUsername(username)
+    await axios
+      .post('https://euapi.alacritys.net/users/login', {
+        email,
+        password: pass,
+        mnemonic_code,
+      })
+      .then(async (response) => {
+        if (response.status === 200) {
+          let activeKey = await Alaio.getUserKeys(
+            mnemonic_code,
+            pass,
+          ).then((res) => res.data.data.activeKey)
+          localStorage.setItem('keys', JSON.stringify(activeKey))
+          let rpc = Alaio.makeRPC()
+          let username = await rpc
+            .history_get_key_accounts(activeKey.pub_key)
+            .then(async (response) => {
+              return response.account_names[0]
+            })
+          if (username !== '') {
+            console.log('res', response)
+            await localStorage.removeItem('User')
+            await setUsername(username)
 
-						let data = {
-							accPrivateKey: response.data.data.accPrivateKey,
-							account: response.data.data.account,
-							mnemonicCode: response.data.data.mnemonicCode,
-							profileInfo: response.data.data.profileInfo,
-							publicKeychain: response.data.data.publicKeychain,
-							username: username,
-						}
-						await localStorage.setItem('User', JSON.stringify(data))
+            let data = {
+              accPrivateKey: response.data.data.accPrivateKey,
+              account: response.data.data.account,
+              mnemonicCode: response.data.data.mnemonicCode,
+              profileInfo: response.data.data.profileInfo,
+              publicKeychain: response.data.data.publicKeychain,
+              username: username,
+            }
+            await localStorage.setItem('User', JSON.stringify(data))
 
-						//?Check to see if the user already has a an account in skillstribe
-						await getUser(username)
-					} else {
-						setError(
-							'Please make sure your credentials are correct.',
-						)
-						return
-					}
-				} else {
-					//! Check to see if the user has already created an account
-					return
-				}
-			})
-			.catch((error) => {
-				console.log('here', error)
-				alert.error(`An error occured: ${error}`)
-				setError('An error occured, please try again')
-			})
-	}
+            //?Check to see if the user already has a an account in skillstribe
+            await getUser(username)
+          } else {
+            setError(
+              'Please make sure your credentials are correct.',
+            )
+            return
+          }
+        } else {
+          //! Check to see if the user has already created an account
+          return
+        }
+      })
+      .catch((error) => {
+        console.log('here', error)
+        alert.error(`An error occured: ${error}`)
+        setError('An error occured, please try again')
+      })
+  }
 
-	const encryptMnemonic = async (mnemonic, password) => {
-		return aladinjs.encryptMnemonic(mnemonic, password)
-	}
+  const encryptMnemonic = async (mnemonic, password) => {
+    return aladinjs.encryptMnemonic(mnemonic, password)
+  }
 
-	useEffect(() => {
-		const user = JSON.parse(localStorage.getItem('User'))
-		if (user === null) {
-			console.log('no data')
-			return
-		}
-	}, [])
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('User'))
+    if (user === null) {
+      console.log('no data')
+      return
+    }
+  }, [])
 
   return (
     <>
@@ -126,7 +126,9 @@ const Create = () => {
           </div>
           <div className="create_details">
             <label htmlFor="">Mnemonic</label>
-            <textarea name="" id="" cols="30" rows="4"></textarea>
+            <textarea placeholder="Mnemonics"
+					value={mnemonic_code}
+					onChange={(e) => set_mnemonic_code(e.target.value)} name="" id="" cols="30" rows="4"></textarea>
             <label htmlFor="">Enter Password</label>
             <input type="search" />
             <Link to="/account">
